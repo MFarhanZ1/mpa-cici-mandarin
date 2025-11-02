84a8be9a6bbf420a7c4eb9ba4694bb7fbcdfd9b9
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2025-11-02 17:53:17 +0700

    🔥feat: Add Hottest News section with new images and integrate into landing page

--- src/components/pages/landing-pages/LearnCulture.astro.tsx
+++ src/components/pages/landing-pages/LearnCulture.astro.tsx
@@ -7,47 +7,49 @@ const vectorImage3 = "/pages/landing-pages/learn-culture/vector-3.png";
 
 export default function LearnCulture() {
   return (
-    <section className="bg-[#FBEDDE] -mt-4 py-6 relative overflow-hidden">
-      <img
-        src={vectorImage1}
-        alt="vectorImage1"
-        className="absolute top-1/6 -left-14 w-24"
-      />
+    <section className="bg-[#FBEDDE] -mt-4 py-6 overflow-hidden">
+      <div className="relative">
+        <img
+          src={vectorImage1}
+          alt="vectorImage1"
+          className="absolute top-1/6 -left-14 w-24"
+        />
 
-      <img
-        src={vectorImage2}
-        alt="vectorImage2"
-        className="absolute top-1/3 -right-10 w-38"
-      />
+        <img
+          src={vectorImage2}
+          alt="vectorImage2"
+          className="absolute top-1/3 -right-10 w-38"
+        />
 
-      <img
-        src={vectorImage3}
-        alt="vectorImage3"
-        className="absolute bottom-1/3 -left-14 w-40 z-20"
-      />
-      <div className="flex flex-col justify-center items-center gap-5">
-        {/* judul dan vist ig */}
-        <div className="flex flex-col px-6 justify-center items-center gap-4">
-          <span className="text-xl text-center font-mochiy-pop-one text-[#CB0D0D]">
-            LEARN CHINESE CULTURE WITH CICI MANDARIN!
-          </span>
-          <div>
-            <button className="bg-[#FFBC2D] py-1 px-3 rounded-lg">
-              <span className="text-[#F03C0B] font-bold">
-                {" "}
-                VISIT OUR INSTAGRAM{" "}
-              </span>
-            </button>
+        <img
+          src={vectorImage3}
+          alt="vectorImage3"
+          className="absolute bottom-1/3 -left-14 w-40 z-20"
+        />
+        <div className="flex flex-col justify-center items-center gap-5">
+          {/* judul dan vist ig */}
+          <div className="flex flex-col px-6 justify-center items-center gap-4">
+            <span className="text-xl text-center font-mochiy-pop-one text-[#CB0D0D]">
+              LEARN CHINESE CULTURE WITH CICI MANDARIN!
+            </span>
+            <div>
+              <button className="bg-[#FFBC2D] py-1 px-3 rounded-lg">
+                <span className="text-[#F03C0B] font-bold">
+                  {" "}
+                  VISIT OUR INSTAGRAM{" "}
+                </span>
+              </button>
+            </div>
           </div>
-        </div>
 
-        <div className="relative flex justify-center items-center">
-          <img
-            src={catImage}
-            alt="catImage"
-            className="w-32 absolute -left-1/4 top-6"
-          />
-          <img src={phoneImage} alt="phoneImage" className="z-10" />
+          <div className="relative flex justify-center items-center">
+            <img
+              src={catImage}
+              alt="catImage"
+              className="w-32 absolute -left-1/4 top-6"
+            />
+            <img src={phoneImage} alt="phoneImage" className="z-20" />
+          </div>
         </div>
       </div>
     </section>
